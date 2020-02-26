27cf66de8413868f4aba6bd4fd292248dbad501d
k-lord <katielord27@gmail.com>
2020-02-26 16:15:59 -0500

    renamed aricle module export

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 // Exporting an object containing all models
 
 module.exports = {
-    Article: require("./Article"),
-    Comment: require("./Comment")
+    Article: require("./article"),
+    Comment: require("./comment")
 };
