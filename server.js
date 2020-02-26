ac0424d1ef3c9db854bcd8a33f5000749904ee53
k-lord <katielord27@gmail.com>
2020-02-26 16:24:29 -0500

    fixed app.listen function that was causing deg deployment errors

--- server.js
+++ server.js
@@ -190,6 +190,6 @@ app.post("/articles/comments/:id", function (req, res) {
 })
 
 // Listen on port 3000
-app.listen(3000, function () {
-    console.log("App running on port 3000!");
-});
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
